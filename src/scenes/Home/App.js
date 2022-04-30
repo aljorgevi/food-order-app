4e39447d4fc0b164fd08bf569b59f1c3f4e409e3
aljorgevi <jormencar@yahoo.com>
2022-04-30 16:00:39 +0100

    add react portal

--- src/scenes/Home/App.js
+++ src/scenes/Home/App.js
@@ -1,9 +1,11 @@
 import Header from '../../components/Header'
 import Meals from '../../components/Meals'
+import Cart from '../../components/Cart'
 
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Meals />
